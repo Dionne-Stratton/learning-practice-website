130708f8b2ed7845ac83d0ae188d20a493c4a512
Dionne-Stratton <68926102+Dionne-Stratton@users.noreply.github.com>
2020-07-28 16:07:06 -0500

    Add files via upload

---
+++ html personal site/index.js
@@ -0,0 +1,9 @@
+button = document.getElementById("shop-button");
+
+button.onmouseover = function() {
+    this.style.backgroundColor = "#00078b";
+  }
+
+button.onmouseout = function() {
+    this.style.backgroundColor = "#3590cc";
+  }
